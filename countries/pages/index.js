227c02827e40a89780be559e0ed6465d3e9ec997
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-08-18 18:28:35 +0300

    uncommitted dependencies and testing

--- countries/pages/index.js
+++ countries/pages/index.js
@@ -1,7 +1,9 @@
-export default function Home() {
+function Home() {
   return (
     <h1 className="text-4xl text-center text-red-600">
       Hello world!
     </h1>
   )
 }
+
+export default Home
--- countries/prisma/seed.js
+++ countries/prisma/seed.js
@@ -1,2 +1,5 @@
 import axios from 'axios'
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
