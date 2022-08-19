65df58f567b2235bebd603e78bfa0b0cca864614
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-08-19 22:04:32 +0300

    fixing imports

--- countries/components/CountryList.js
+++ countries/components/CountryList.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react"
-import Country from "./Country"
-import LoadMore from './LoadMore'
+import { useState, useEffect } from 'react';
+import Country from './Country';
+import LoadMore from './LoadMore';
 
 const CountryList = ({ countries, searchText }) => {
   const defaultStep = 10
--- countries/components/layout.js
+++ countries/components/layout.js
@@ -1,6 +1,6 @@
 const Layout = ({ children }) => {
   return (
-    <div className="p-5">
+    <div className='p-5'>
       <header>
         <h1 className='text-4xl font-semibold text-center mb-10 text-gray-900 dark:text-white'>
           Countries
