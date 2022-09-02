5a9bdc6b64d9c374b3b2a21d1470e323cef52ca1
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-09-02 04:58:07 +0300

    Added tRPC to the Next.js project

--- countries/utils/trpc.ts
+++ countries/utils/trpc.ts
@@ -1,5 +1,4 @@
 import { createReactQueryHooks } from '@trpc/react'
 import type { AppRouter } from '../server/routers/_app'
-import type { inferProcedureInput, inferProcedureOutput } from '@trpc/server';
 
 export const trpc = createReactQueryHooks<AppRouter>()
