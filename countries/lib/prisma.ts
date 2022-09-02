e41e40963d8ae780d0decdee1c4d9d0e79efa81b
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-09-03 00:47:36 +0300

    Removed prisma logging

--- countries/lib/prisma.ts
+++ countries/lib/prisma.ts
@@ -13,10 +13,7 @@ declare global {
 }
 
 export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
+  global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma
