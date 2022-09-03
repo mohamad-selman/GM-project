439c06af550377dbd69998995881653ec35da29f
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-09-04 01:52:46 +0300

    Fixed tRPC configuration in _app.tsx

--- countries/pages/_app.tsx
+++ countries/pages/_app.tsx
@@ -15,16 +15,15 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 }
 
 export default withTRPC<AppRouter>({
-  config() {
+  config({ ctx }) {
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-     const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc'
     return {
-      url,
+      url: process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}/api/trpc`
+        : 'http://localhost:3000/api/trpc',
       transformer: superjson,
       /**
        * @link https://react-query.tanstack.com/reference/QueryClient
