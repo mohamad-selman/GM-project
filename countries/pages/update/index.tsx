6ff2958fb63e2a8a020ec2730bb086cb3164050d
Mahmoud <mhd-10@hotmail.com>
2022-09-06 09:21:23 +0300

    Updated CardComponent
    try always to have a composable components that can be used in many ways
    CountryCard can only be used to display Countries.
    in this small project its fine, but in large codebases
    it causes alot of issues
    - redundant
    - hard to manage and maintain
    
    read about component composition

---
+++ countries/pages/update/index.tsx
@@ -0,0 +1,114 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { Fragment } from 'react';
+import Search from '../../components/Search';
+import LoadMore from '../../components/LoadMore';
+import { findCountries } from '../../server/findCountries';
+import { trpc } from '../../utils/trpc';
+import { Country } from '@prisma/client';
+import { useForm } from 'react-hook-form';
+import { Grid, Box, Typography, Container, Button } from '@mui/material';
+import { CustomCard } from '../../components/update/CustomCard';
+
+interface Props {
+	initialData: {
+		pages: {
+			items: Country[];
+			nextCursor: number | undefined;
+		}[];
+	};
+}
+
+const Home: NextPage<Props> = ({ initialData }) => {
+	const { register, watch, handleSubmit } = useForm();
+	const { data, isFetchingNextPage, fetchNextPage, hasNextPage } = trpc.useInfiniteQuery(
+		[
+			'infiniteCountries',
+			{
+				limit: 10,
+				name: watch('name', ''),
+			},
+		],
+		{
+			initialData: {
+				pageParams: [undefined],
+				...initialData,
+			},
+			getNextPageParam: (lastPage) => lastPage.nextCursor,
+			refetchOnMount: false,
+		}
+	);
+
+	return (
+		<Container sx={{ py: 4 }} maxWidth="xl">
+			<Head>
+				<title>Countries</title>
+			</Head>
+
+			<Typography sx={{ textAlign: 'center' }} variant="h4" fontWeight="fontWeightMedium">
+				Countries
+			</Typography>
+
+			<Search register={register} handleSubmit={handleSubmit} />
+
+			<Box sx={{ flexGrow: 1 }}>
+				<Grid container spacing={{ xs: 2, md: 3 }}>
+					{data?.pages.map((page) => (
+						<Fragment key={page.nextCursor}>
+							{page.items.map((country, idx) => (
+								<Grid key={idx} item xs={12} sm={6} md={4} lg={3}>
+									<CustomCard
+										img={country.flag}
+										alt={country.name}
+										action={<> {country.map && <Button href={country.map}>Map</Button>}</>}
+									>
+										<Typography gutterBottom variant="h5" component="div">
+											{country.name}
+										</Typography>
+										<Typography variant="body2" color="text.secondary">
+											<strong>Capital:</strong> {country.capital || 'N/A'}
+										</Typography>
+										<Typography variant="body2" color="text.secondary">
+											<strong>Region:</strong> {country.region || 'N/A'}
+										</Typography>
+										<Typography variant="body2" color="text.secondary">
+											<strong>Population:</strong> {country.population || 'N/A'}
+										</Typography>
+										<Typography variant="body2" color="text.secondary">
+											<strong>Language:</strong> {country.language || 'N/A'}
+										</Typography>
+									</CustomCard>
+								</Grid>
+							))}
+						</Fragment>
+					))}
+				</Grid>
+			</Box>
+
+			<LoadMore fetchNextPage={fetchNextPage} hasNextPage={hasNextPage} isFetchingNextPage={isFetchingNextPage} />
+		</Container>
+	);
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const { items, nextCursor } = await findCountries({
+		limit: 10,
+		where: {},
+	});
+
+	return {
+		props: {
+			initialData: {
+				pages: [
+					{
+						items: items,
+						nextCursor: nextCursor,
+					},
+				],
+			},
+		},
+	};
+};
+
+export default Home;
