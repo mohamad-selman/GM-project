bc4c38415f84f69c3c8a27ef74ee78490a6a436a
Mahmoud <mhd-10@hotmail.com>
2022-09-06 08:24:48 +0300

    added tsconfig rule noUnusedLocals
    - this should raise typescript error if there is unused variable or import

--- countries/pages/index.tsx
+++ countries/pages/index.tsx
@@ -1,113 +1,103 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { GetStaticProps } from 'next'
-import { Fragment } from 'react'
-import Search from '../components/Search'
-import CountryCard from '../components/CountryCard'
-import LoadMore from '../components/LoadMore'
-import { findCountries } from '../server/findCountries'
-import { trpc } from '../utils/trpc'
-import { Country } from '@prisma/client'
-import { useForm } from 'react-hook-form'
-import { Grid, Box, Typography, Container } from '@mui/material'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { Fragment } from 'react';
+import Search from '../components/Search';
+import CountryCard from '../components/CountryCard';
+import LoadMore from '../components/LoadMore';
+import { findCountries } from '../server/findCountries';
+import { trpc } from '../utils/trpc';
+import { Country } from '@prisma/client';
+import { useForm } from 'react-hook-form';
+import { Grid, Box, Typography, Container } from '@mui/material';
 
 interface Props {
-  initialData: {
-    pages: {
-      items: Country[]
-      nextCursor: number | undefined
-    }[]
-  }
+	initialData: {
+		pages: {
+			items: Country[];
+			nextCursor: number | undefined;
+		}[];
+	};
 }
 
 const Home: NextPage<Props> = ({ initialData }) => {
-  const { register, watch, handleSubmit } = useForm()
-  const {
-    status,
-    data,
-    error,
-    isFetching,
-    isFetchingNextPage,
-    fetchNextPage,
-    hasNextPage,
-  } = trpc.useInfiniteQuery(
-    ['infiniteCountries', {
-      limit: 10,
-      name: watch('name', ''),
-    }],
-    {
-      initialData: ({
-        pageParams: [undefined],
-        ...initialData,
-      }),
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-      refetchOnMount: false,
-    },
-  )
+	const { register, watch, handleSubmit } = useForm();
+	const { data, isFetchingNextPage, fetchNextPage, hasNextPage } = trpc.useInfiniteQuery(
+		[
+			'infiniteCountries',
+			{
+				limit: 10,
+				name: watch('name', ''),
+			},
+		],
+		{
+			initialData: {
+				pageParams: [undefined],
+				...initialData,
+			},
+			getNextPageParam: (lastPage) => lastPage.nextCursor,
+			refetchOnMount: false,
+		}
+	);
 
-  return (
-    <Container sx={{ py: 4 }} maxWidth='xl'>
-      <Head>
-        <title>Countries</title>
-      </Head>
+	return (
+		<Container sx={{ py: 4 }} maxWidth="xl">
+			<Head>
+				<title>Countries</title>
+			</Head>
 
-      <Typography sx={{textAlign: 'center'}} variant='h4' fontWeight='fontWeightMedium'>
-        Countries
-      </Typography>
+			<Typography sx={{ textAlign: 'center' }} variant="h4" fontWeight="fontWeightMedium">
+				Countries
+			</Typography>
 
-      <Search
-        register={register}
-        handleSubmit={handleSubmit}
-      />
+			<Search register={register} handleSubmit={handleSubmit} />
 
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={{ xs: 2, md: 3 }}>
-          {data?.pages.map((page) => (
-            <Fragment key={page.nextCursor}>
-              {page.items.map((country, idx) => (
-                <Grid key={idx} item xs={12} sm={6} md={4} lg={3}>
-                  <CountryCard
-                    key={idx}
-                    name={country.name}
-                    flag={country.flag}
-                    capital={country.capital}
-                    region={country.region}
-                    population={country.population}
-                    language={country.language}
-                    map={country.map}
-                  />
-                </Grid>
-              ))}
-            </Fragment>
-          ))}
-        </Grid>
-      </Box>
+			<Box sx={{ flexGrow: 1 }}>
+				<Grid container spacing={{ xs: 2, md: 3 }}>
+					{data?.pages.map((page) => (
+						<Fragment key={page.nextCursor}>
+							{page.items.map((country, idx) => (
+								<Grid key={idx} item xs={12} sm={6} md={4} lg={3}>
+									<CountryCard
+										key={idx}
+										name={country.name}
+										flag={country.flag}
+										capital={country.capital}
+										region={country.region}
+										population={country.population}
+										language={country.language}
+										map={country.map}
+									/>
+								</Grid>
+							))}
+						</Fragment>
+					))}
+				</Grid>
+			</Box>
 
-      <LoadMore
-        fetchNextPage={fetchNextPage}
-        hasNextPage={hasNextPage}
-        isFetchingNextPage={isFetchingNextPage}
-      />
-    </Container>
-  )
-}
+			<LoadMore fetchNextPage={fetchNextPage} hasNextPage={hasNextPage} isFetchingNextPage={isFetchingNextPage} />
+		</Container>
+	);
+};
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { items, nextCursor } = await findCountries({
-    limit: 10,
-    where: {},
-  })
+	const { items, nextCursor } = await findCountries({
+		limit: 10,
+		where: {},
+	});
 
-  return {
-    props: {
-      initialData: {
-        pages: [{
-          items: items,
-          nextCursor: nextCursor
-        }]
-      }
-    }
-  }
-}
+	return {
+		props: {
+			initialData: {
+				pages: [
+					{
+						items: items,
+						nextCursor: nextCursor,
+					},
+				],
+			},
+		},
+	};
+};
 
-export default Home
+export default Home;
